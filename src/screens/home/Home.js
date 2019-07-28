68665d7d5a06e6edb5eb5c2a51571f6d3d3764b1
Aman kumar <aman@Amans-MacBook-Air.local>
2019-07-28 12:39:15 +0530

    added home component

---
+++ src/screens/home/Home.js
@@ -0,0 +1,12 @@
+import React, {component} from 'react';
+import './Home.css';
+
+class Home extends component{
+    render() {
+        return(
+            <div></div>
+        )
+    }
+}
+
+export default Home;
